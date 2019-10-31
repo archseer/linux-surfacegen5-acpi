53f683159d1107a7a4b28c52a67987c74c5ac9cb
Blaž Hrastnik <blaz@mxxn.io>
2019-10-31 12:25:08 +0900

    VHF: Add support for Surface Laptop 3 keyboard & touchpad.

--- scripts/sysfs_rqst.py
+++ scripts/sysfs_rqst.py
@@ -11,7 +11,6 @@ PATH_DEV_RQST = '/sys/bus/serial/devices/serial0-0/rqst'
 # detach abort   [0x11, 0x08, 0x00, 0x01, 0x00, 0x00]
 # detach ack     [0x11, 0x09, 0x00, 0x01, 0x00, 0x00]
 
-
 def performance_state_request(state):
     return bytes([0x03, 0x03, 0x00, 0x01, 0x00, 0x04, state, 0x00, 0x00, 0x00])
 
@@ -20,17 +19,17 @@ def main():
     fd = os.open(PATH_DEV_RQST, os.O_RDWR | os.O_SYNC)
 
     #            [  TC,  CID,  IID,  PRI,  SNC,  CDL, payload...]
-    data = bytes([0x02, 0x01, 0x03, 0x02, 0x01, 0x00])
+    data = bytes([0x15, 0x03, 0x03, 0x02, 0x00, 0x02, 0x07, 0x03])
 
+    fd = os.open(path, os.O_RDWR | os.O_SYNC)
     os.write(fd, data)
 
     os.lseek(fd, 0, os.SEEK_SET)
     length = os.read(fd, 1)[0]
     data = os.read(fd, length)
 
-    os.close(fd)
-
     print(' '.join(['{:02x}'.format(x) for x in data]))
+    finally:
 
 
 if __name__ == '__main__':
